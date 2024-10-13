8ac4faf27314fa44ff6635a7bd1a091ea621b5d3
DrBrown28 <112201610+DrBrown28@users.noreply.github.com>
2024-10-13 12:32:16 -0400

    NX659J: Adapt tree to Lmodroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nx659j.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_nx659j-eng \
-    lineage_nx659j-user \
-    lineage_nx659j-userdebug
+    $(LOCAL_DIR)/lmodroid_nx659j.mk
